a396420f1111358e000a5ad2d4481ec9f603e2bc
tla <756459364@qq.com>
2020-10-24 22:17:05 +0800

    coding to 14.1.5

--- ship.py
+++ ship.py
@@ -37,3 +37,6 @@ class Ship():
         """draw the ship at its current location"""
         self.screen.blit(self.image,self.rect)
 
+    def center_ship(self):
+        """ 让飞船在屏幕上居中 """
+        self.center = self.screen_rect.centerx
